1efd41237fda5b45abd2502f058aa14f2aaac6c6
Blake Mason & Mark Rubenstein <student@epicodus.com>
2014-08-25 10:18:42 -0700

    tables &  specs for associations

---
+++ db/migrate/20140825170315_new_bus_timetable.rb
@@ -0,0 +1,14 @@
+class NewBusTimetable < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.string :name
+    end
+    create_table :lines do |t|
+      t.string :name
+    end
+    create_table :stops do |t|
+      t.belongs_to :station
+      t.belongs_to :line
+    end
+  end
+end
---
+++ spec/station_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Station do
+  it {should have_many :lines}
+  it {should have_many :stops}
+end
